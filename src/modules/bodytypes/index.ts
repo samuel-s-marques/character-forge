638853bea94f4c9779c68a004972b3e0efbc1fe3
Samuel Marques <samuel.s.marques@protonmail.com>
2023-08-24 20:02:22 -0300

    fix bugs

--- src/modules/bodytypes/index.ts
+++ src/modules/bodytypes/index.ts
@@ -1,16 +1,36 @@
 import { processFile } from "../../utils/utils";
+import { Sex } from "../sexes";
 
-export interface BodyType {}
+export interface BodyType {
+  type: string;
+  height: number;
+  weight: number;
+}
 
 export class BodyTypesModule {
   private loadBodyTypeData(): any {
     return processFile("bodytypes")["data"];
   }
 
-  public getRandomBodyType(): BodyType {
+  public getRandomBodyType(sex: Sex): BodyType {
     const bodyTypes = this.loadBodyTypeData();
-    const randomIndex = Math.floor(Math.random() * bodyTypes.length);
+    const keys = Object.keys(bodyTypes[sex.toString()]);
 
-    return bodyTypes[randomIndex];
+    const randomIndex = Math.floor(Math.random() * keys.length);
+    const type = keys[randomIndex];
+
+    const { heightRange, weightRange } =
+      bodyTypes[sex.toString()][keys[randomIndex]];
+
+    const height =
+      Math.random() * (heightRange[1] - heightRange[0]) + heightRange[0];
+    const weight =
+      Math.random() * (weightRange[1] - weightRange[0]) + weightRange[0];
+
+    return {
+      type,
+      height,
+      weight,
+    };
   }
 }
