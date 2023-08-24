d5a15be060f9f654331241dd2737b13e41d0c295
Samuel Marques <samuel.s.marques@protonmail.com>
2023-08-23 22:08:15 -0300

    Create index.ts

---
+++ src/index.ts
@@ -0,0 +1,25 @@
+import { EyeColorsModule } from "./modules/eyecolors";
+import { HairColorsModule } from "./modules/haircolors";
+import { HairStylesModule } from "./modules/hairstyles";
+import { NamesModule } from "./modules/names";
+import { OccupationsModule } from "./modules/occupations";
+import {
+  PersonalityTraitModule,
+} from "./modules/personalitytraits";
+import { SexesModule } from "./modules/sexes";
+import { SocialClassesModule } from "./modules/socialclasses";
+import { SurnamesModule } from "./modules/surnames";
+import { CharacterForge } from "./character-forge";
+
+export {
+  EyeColorsModule,
+  HairColorsModule,
+  HairStylesModule,
+  NamesModule,
+  OccupationsModule,
+  PersonalityTraitModule,
+  SexesModule,
+  SocialClassesModule,
+  SurnamesModule,
+  CharacterForge,
+};
