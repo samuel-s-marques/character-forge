c8c70021b3c11d3f593fd044057f6881eec058e1
Samuel Marques <samuel.s.marques@protonmail.com>
2023-08-25 01:16:31 -0300

    add nicknames

--- src/modules/names/index.ts
+++ src/modules/names/index.ts
@@ -1,7 +1,10 @@
 import { processFile } from "../../utils/utils";
 import { Sex } from "../sexes";
 
-export interface Name {}
+export interface Name {
+  name: string;
+  nickname: string;
+}
 
 export class NamesModule {
   private loadNamesData(): any {
@@ -9,9 +12,18 @@ export class NamesModule {
   }
 
   public getRandomName(sex: Sex): Name {
-    const names = this.loadNamesData().find((entry: { [x: string]: any }) => entry[sex.toString()]);
-    const randomIndex = Math.floor(Math.random() * names[sex.toString()].length)
+    const names = this.loadNamesData().find(
+      (entry: { [x: string]: any }) => entry[sex.toString()]
+    );
 
-    return names[sex.toString()][randomIndex];
+    const randomIndex = Math.floor(
+      Math.random() * names[sex.toString()].length
+    );
+    const { name, nicknames } = names[sex.toString()][randomIndex];
+
+    const randomNicknameIndex = Math.floor(Math.random() * nicknames.length);
+    const nickname = nicknames[randomNicknameIndex];
+
+    return { name, nickname };
   }
 }
