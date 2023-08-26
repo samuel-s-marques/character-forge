585998874cee25cc83d8de88813030d60d3be142
Samuel Marques <samuel.s.marques@protonmail.com>
2023-08-26 19:07:34 -0300

    implement modularization

--- src/character-forge.ts
+++ src/character-forge.ts
@@ -1,113 +1,44 @@
-import { Age, AgesModule } from "./modules/age";
-import { BodyType, BodyTypesModule } from "./modules/bodytypes";
-import { EthnicitiesModule, Ethnicity } from "./modules/ethnicities";
-import { EyeColor, EyeColorsModule } from "./modules/eyecolors";
-import { HairColor, HairColorsModule } from "./modules/haircolors";
-import { HairStyle, HairStylesModule } from "./modules/hairstyles";
-import { Hobbies, HobbiesModule } from "./modules/hobbies";
-import { Name, NamesModule } from "./modules/names";
-import { Occupation, OccupationsModule } from "./modules/occupations";
-import {
-  PersonalityTrait,
-  PersonalityTraitModule,
-} from "./modules/personalitytraits";
-import { Sex, SexesModule } from "./modules/sexes";
-import { SexualitiesModule, Sexuality } from "./modules/sexualities";
-import { SocialClass, SocialClassesModule } from "./modules/socialclasses";
-import { Surname, SurnamesModule } from "./modules/surnames";
-import { processFile } from "./utils/utils";
+import fs from "fs";
+import path from "path";
 
-export interface Character {
-  name: string;
-  nickname: string;
-  surname: Surname;
-  sex: Sex;
-  age: Age;
-  hairColor: HairColor;
-  eyeColor: EyeColor;
-  hairStyle: HairStyle;
-  bodyType: string;
-  height: number;
-  weight: number;
-  ethnicity: Ethnicity;
-  sexuality: Sexuality;
-  occupation: Occupation;
-  personalityTraits: PersonalityTrait[];
-  socialClass: SocialClass;
-  hobbies: Hobbies;
-}
+export interface Character {}
 
 export class CharacterForge {
-  private namesModule: NamesModule;
-  private surnamesModule: SurnamesModule;
-  private sexesModule: SexesModule;
-  private agesModule: AgesModule;
-  private hairColorsModule: HairColorsModule;
-  private eyeColorsModule: EyeColorsModule;
-  private hairStylesModule: HairStylesModule;
-  private occupationsModule: OccupationsModule;
-  private personalityTraitsModule: PersonalityTraitModule;
-  private socialClassesModule: SocialClassesModule;
-  private bodyTypesModule: BodyTypesModule;
-  private ethnicitiesModule: EthnicitiesModule;
-  private sexualitiesModule: SexualitiesModule;
-  private hobbiesModule: HobbiesModule;
+  private loadedModules: Record<string, any> = {};
 
-  constructor() {
-    this.namesModule = new NamesModule();
-    this.surnamesModule = new SurnamesModule();
-    this.sexesModule = new SexesModule();
-    this.agesModule = new AgesModule();
-    this.hairColorsModule = new HairColorsModule();
-    this.eyeColorsModule = new EyeColorsModule();
-    this.hairStylesModule = new HairStylesModule();
-    this.occupationsModule = new OccupationsModule();
-    this.personalityTraitsModule = new PersonalityTraitModule();
-    this.socialClassesModule = new SocialClassesModule();
-    this.bodyTypesModule = new BodyTypesModule();
-    this.ethnicitiesModule = new EthnicitiesModule();
-    this.sexualitiesModule = new SexualitiesModule();
-    this.hobbiesModule = new HobbiesModule();
+  /*
+  * By implementing concurrency succesfully, the modules would be read
+  * and imported much faster.
+  */
+  private async moduleLoader() {
+    const folders = fs.readdirSync(path.join(__dirname, "modules"));
+
+    for (const folder of folders) {
+      const module = await import(`./modules/${folder}/index.js`);
+      const moduleName = folder.format();
+      const moduleInstance = new module[`${moduleName}Module`]();
+      const moduleInterface = moduleInstance["interfaceName"];
+
+      this.loadedModules[moduleInterface] = moduleInstance;
+    }
   }
 
-  public forge(): Character {
-    const sex = this.sexesModule.getRandomSex();
-    const { name, nickname } = this.namesModule.getRandomName(sex);
-    const surname = this.surnamesModule.getRandomSurname();
-    const age = this.agesModule.getRandomAge(18, 50);
-    const hairColor = this.hairColorsModule.getRandomHairColor();
-    const eyeColor = this.eyeColorsModule.getRandomEyeColor();
-    const hairStyle = this.hairStylesModule.getRandomHairStyle();
-    const occupation = this.occupationsModule.getRandomOccupation();
-    const personalityTraits = this.personalityTraitsModule.pickRandomTrait(5);
-    const socialClass = this.socialClassesModule.getRandomSocialClass();
-    const bodyTypeData = this.bodyTypesModule.getRandomBodyType(sex);
-    const bodyType = bodyTypeData.type;
-    const height = bodyTypeData.height;
-    const weight = bodyTypeData.weight;
-    const ethnicity = this.ethnicitiesModule.getRandomEthnicity();
-    const sexuality = this.sexualitiesModule.getRandomSexuality();
-    const hobbies = this.hobbiesModule.getRandomHobbies();
+  /*
+  * There's a problem here: I really don't know how to pass parameters
+  * to the modules.
+  * 
+  * Some modules have parameters, like `Name` and `BodyType`.
+  */ 
+  public forge(): Record<string, any> {
+    let character: Record<string, any> = {};
 
-    const character: Character = {
-      name,
-      nickname,
-      surname,
-      sex,
-      age,
-      hairColor,
-      eyeColor,
-      hairStyle,
-      bodyType,
-      height,
-      weight,
-      ethnicity,
-      sexuality,
-      occupation,
-      personalityTraits,
-      socialClass,
-      hobbies,
-    };
+    this.moduleLoader().then(() => {
+      for (const moduleInterface in this.loadedModules) {
+        const module = moduleInterface.toLocaleLowerCase();
+
+        character[module] = this.loadedModules[`getRandom${moduleInterface}`]();
+      }
+    });
 
     return character;
   }
