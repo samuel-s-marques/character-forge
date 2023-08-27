6e5db54188322b807261c17c5377d935b92e2bf3
Samuel Marques <samuel.s.marques@protonmail.com>
2023-08-27 20:18:54 -0300

    remove Math.floor and add method

--- src/modules/age/index.ts
+++ src/modules/age/index.ts
@@ -1,7 +1,9 @@
+import { generateRandomNumber } from "../../utils/utils";
+
 export interface Age {}
 
 export class AgesModule {
   public getRandomAge(min: number = 18, max: number = 50): Age {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
+    return generateRandomNumber(min, max);
   }
 }
