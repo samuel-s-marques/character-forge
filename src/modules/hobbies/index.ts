ba1118a6acb712c205d808d88ea4d54a1803ce37
Samuel Marques <samuel.s.marques@protonmail.com>
2023-08-27 16:39:33 -0300

    add min max

--- src/modules/hobbies/index.ts
+++ src/modules/hobbies/index.ts
@@ -7,10 +7,10 @@ export class HobbiesModule {
     return processFile("hobbies")["data"];
   }
 
-  public getRandomHobbies(): Hobbies {
+  public getRandomHobbies(min: number = 1, max: number = 3): Hobbies {
     const hobbies = this.loadHobbiesData();
     const selectedHobbies: string[] = [];
-    const numItemsToPick = Math.floor(Math.random() * 3) + 1;
+    const numItemsToPick = Math.floor(Math.random() * (max - min + 1)) + min;
 
     for (let index = 0; index < numItemsToPick; index++) {
       const randomIndex = Math.floor(Math.random() * hobbies.length);
