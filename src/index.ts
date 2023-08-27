f37a9628203130ea44f81b2e4267ea97eb125146
Samuel Marques <samuel.s.marques@protonmail.com>
2023-08-27 15:27:24 -0300

    reorder imports and exports

--- src/index.ts
+++ src/index.ts
@@ -1,35 +1,35 @@
+import { AgesModule } from "./modules/age";
+import { AlignmentsModule } from "./modules/alignments";
+import { BodyTypesModule } from "./modules/bodytypes";
+import { CharacterForge } from "./character-forge";
+import { EthnicitiesModule } from "./modules/ethnicities";
 import { EyeColorsModule } from "./modules/eyecolors";
 import { HairColorsModule } from "./modules/haircolors";
 import { HairStylesModule } from "./modules/hairstyles";
+import { HobbiesModule } from "./modules/hobbies";
 import { NamesModule } from "./modules/names";
 import { OccupationsModule } from "./modules/occupations";
 import { PersonalityTraitModule } from "./modules/personalitytraits";
 import { SexesModule } from "./modules/sexes";
+import { SexualitiesModule } from "./modules/sexualities";
 import { SocialClassesModule } from "./modules/socialclasses";
 import { SurnamesModule } from "./modules/surnames";
-import { CharacterForge } from "./character-forge";
-import { BodyTypesModule } from "./modules/bodytypes";
-import { EthnicitiesModule } from "./modules/ethnicities";
-import { HobbiesModule } from "./modules/hobbies";
-import { AgesModule } from "./modules/age";
-import { SexualitiesModule } from "./modules/sexualities";
-import { AlignmentsModule } from "./modules/alignments";
 
 export {
   AgesModule,
   AlignmentsModule,
-  SexualitiesModule,
+  BodyTypesModule,
+  CharacterForge,
+  EthnicitiesModule,
   EyeColorsModule,
   HairColorsModule,
   HairStylesModule,
+  HobbiesModule,
   NamesModule,
   OccupationsModule,
   PersonalityTraitModule,
   SexesModule,
+  SexualitiesModule,
   SocialClassesModule,
   SurnamesModule,
-  BodyTypesModule,
-  EthnicitiesModule,
-  HobbiesModule,
-  CharacterForge,
 };
