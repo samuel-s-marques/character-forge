6bdafac340e697f0f92bb5aaaa35041dbb5633c6
Samuel Marques <samuel.s.marques@protonmail.com>
2023-08-27 15:26:04 -0300

    remove imports

--- src/character-forge.ts
+++ src/character-forge.ts
@@ -1,74 +1,23 @@
-import { Age, AgesModule } from "./modules/age";
-import { BodyType, BodyTypesModule } from "./modules/bodytypes";
-import { EthnicitiesModule, Ethnicity } from "./modules/ethnicities";
-import { EyeColor, EyeColorsModule } from "./modules/eyecolors";
-import { HairColor, HairColorsModule } from "./modules/haircolors";
-import { HairStyle, HairStylesModule } from "./modules/hairstyles";
-import { Hobbies, HobbiesModule } from "./modules/hobbies";
-import { Name, NamesModule } from "./modules/names";
-import { Occupation, OccupationsModule } from "./modules/occupations";
-import {
-  PersonalityTrait,
-  PersonalityTraitModule,
-} from "./modules/personalitytraits";
-import { Sex, SexesModule } from "./modules/sexes";
-import { SexualitiesModule, Sexuality } from "./modules/sexualities";
-import { SocialClass, SocialClassesModule } from "./modules/socialclasses";
-import { Surname, SurnamesModule } from "./modules/surnames";
-import { processFile } from "./utils/utils";
+import * as mod from "./index";
 
-export interface Character {
-  name: string;
-  nickname: string;
-  surname: Surname;
-  sex: Sex;
-  age: Age;
-  hairColor: HairColor;
-  eyeColor: EyeColor;
-  hairStyle: HairStyle;
-  bodyType: string;
-  height: number;
-  weight: number;
-  ethnicity: Ethnicity;
-  sexuality: Sexuality;
-  occupation: Occupation;
-  personalityTraits: PersonalityTrait[];
-  socialClass: SocialClass;
-  hobbies: Hobbies;
-}
+export interface Character {}
 
 export class CharacterForge {
-  private namesModule: NamesModule;
-  private surnamesModule: SurnamesModule;
-  private sexesModule: SexesModule;
-  private agesModule: AgesModule;
-  private hairColorsModule: HairColorsModule;
-  private eyeColorsModule: EyeColorsModule;
-  private hairStylesModule: HairStylesModule;
-  private occupationsModule: OccupationsModule;
-  private personalityTraitsModule: PersonalityTraitModule;
-  private socialClassesModule: SocialClassesModule;
-  private bodyTypesModule: BodyTypesModule;
-  private ethnicitiesModule: EthnicitiesModule;
-  private sexualitiesModule: SexualitiesModule;
-  private hobbiesModule: HobbiesModule;
-
-  constructor() {
-    this.namesModule = new NamesModule();
-    this.surnamesModule = new SurnamesModule();
-    this.sexesModule = new SexesModule();
-    this.agesModule = new AgesModule();
-    this.hairColorsModule = new HairColorsModule();
-    this.eyeColorsModule = new EyeColorsModule();
-    this.hairStylesModule = new HairStylesModule();
-    this.occupationsModule = new OccupationsModule();
-    this.personalityTraitsModule = new PersonalityTraitModule();
-    this.socialClassesModule = new SocialClassesModule();
-    this.bodyTypesModule = new BodyTypesModule();
-    this.ethnicitiesModule = new EthnicitiesModule();
-    this.sexualitiesModule = new SexualitiesModule();
-    this.hobbiesModule = new HobbiesModule();
-  }
+  private namesModule = new mod.NamesModule();
+  private surnamesModule = new mod.SurnamesModule();
+  private sexesModule = new mod.SexesModule();
+  private agesModule = new mod.AgesModule();
+  private hairColorsModule = new mod.HairColorsModule();
+  private eyeColorsModule = new mod.EyeColorsModule();
+  private hairStylesModule = new mod.HairStylesModule();
+  private occupationsModule = new mod.OccupationsModule();
+  private personalityTraitsModule = new mod.PersonalityTraitModule();
+  private socialClassesModule = new mod.SocialClassesModule();
+  private bodyTypesModule = new mod.BodyTypesModule();
+  private ethnicitiesModule = new mod.EthnicitiesModule();
+  private sexualitiesModule = new mod.SexualitiesModule();
+  private hobbiesModule = new mod.HobbiesModule();
+  private alignmentsModule = new mod.AlignmentsModule();
 
   public forge(): Character {
     const sex = this.sexesModule.getRandomSex();
@@ -88,6 +37,7 @@ export class CharacterForge {
     const ethnicity = this.ethnicitiesModule.getRandomEthnicity();
     const sexuality = this.sexualitiesModule.getRandomSexuality();
     const hobbies = this.hobbiesModule.getRandomHobbies();
+    const alignment = this.alignmentsModule.getRandomAlignment();
 
     const character: Character = {
       name,
@@ -107,6 +57,7 @@ export class CharacterForge {
       personalityTraits,
       socialClass,
       hobbies,
+      alignment,
     };
 
     return character;
