260ce7a825c955392dfd1a02f70ca2e06d63cab5
Samuel Marques <samuel.s.marques@protonmail.com>
2023-08-27 15:26:13 -0300

    import hobbies, ages, sexualities and alignments

--- src/index.ts
+++ src/index.ts
@@ -3,17 +3,22 @@ import { HairColorsModule } from "./modules/haircolors";
 import { HairStylesModule } from "./modules/hairstyles";
 import { NamesModule } from "./modules/names";
 import { OccupationsModule } from "./modules/occupations";
-import {
-  PersonalityTraitModule,
-} from "./modules/personalitytraits";
+import { PersonalityTraitModule } from "./modules/personalitytraits";
 import { SexesModule } from "./modules/sexes";
 import { SocialClassesModule } from "./modules/socialclasses";
 import { SurnamesModule } from "./modules/surnames";
 import { CharacterForge } from "./character-forge";
 import { BodyTypesModule } from "./modules/bodytypes";
 import { EthnicitiesModule } from "./modules/ethnicities";
+import { HobbiesModule } from "./modules/hobbies";
+import { AgesModule } from "./modules/age";
+import { SexualitiesModule } from "./modules/sexualities";
+import { AlignmentsModule } from "./modules/alignments";
 
 export {
+  AgesModule,
+  AlignmentsModule,
+  SexualitiesModule,
   EyeColorsModule,
   HairColorsModule,
   HairStylesModule,
@@ -25,5 +30,6 @@ export {
   SurnamesModule,
   BodyTypesModule,
   EthnicitiesModule,
+  HobbiesModule,
   CharacterForge,
 };
