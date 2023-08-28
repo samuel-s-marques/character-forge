3c95392f00b98a9d40b661b4e17ef6adfd490fc4
Samuel Marques <samuel.s.marques@protonmail.com>
2023-08-27 21:35:02 -0300

    add interfaces

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import { AgesModule } from "./modules/age";
 import { AlignmentsModule } from "./modules/alignments";
-import { BodyTypesModule } from "./modules/bodytypes";
+import { BodyTypesModule, BodyType } from "./modules/bodytypes";
 import { CharacterForge } from "./character-forge";
 import { EthnicitiesModule } from "./modules/ethnicities";
 import { EyeColorsModule } from "./modules/eyecolors";
@@ -10,22 +10,24 @@ import { HobbiesModule } from "./modules/hobbies";
 import { MaritalStatusesModule } from "./modules/maritalstatus";
 import { NamesModule } from "./modules/names";
 import { OccupationsModule } from "./modules/occupations";
-import { PersonalityTraitModule } from "./modules/personalitytraits";
+import { PersonalityTraitModule, PersonalityTrait } from "./modules/personalitytraits";
 import { SexesModule } from "./modules/sexes";
 import { SexualitiesModule } from "./modules/sexualities";
 import { SocialClassesModule } from "./modules/socialclasses";
 import { SurnamesModule } from "./modules/surnames";
 import { PhobiasModule } from "./modules/phobias";
 import { PoliticsModule } from "./modules/politics";
-import { ClothingsModule } from "./modules/clothing";
+import { ClothingsModule, Clothing } from "./modules/clothing";
 
 export {
   AgesModule,
   AlignmentsModule,
   BodyTypesModule,
+  BodyType,
   CharacterForge,
   EthnicitiesModule,
   ClothingsModule,
+  Clothing,
   PhobiasModule,
   EyeColorsModule,
   HairColorsModule,
@@ -36,8 +38,10 @@ export {
   NamesModule,
   OccupationsModule,
   PersonalityTraitModule,
+  PersonalityTrait,
   SexesModule,
   SexualitiesModule,
   SocialClassesModule,
   SurnamesModule,
+  
 };
