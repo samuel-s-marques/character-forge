73e124f58f9c33962d325147a352fb57b28b423c
Samuel Marques <samuel.s.marques@protonmail.com>
2023-08-28 13:45:32 -0300

    implement pronoun interface

--- src/modules/pronouns/index.ts
+++ src/modules/pronouns/index.ts
@@ -1,11 +1,18 @@
 import { processFile } from "../../utils/utils";
 
+export interface Pronoun {
+  subjectPronoun: string;
+  objectPronoun: string;
+  possessiveAdjective: string;
+  possessivePronoun: string;
+}
+
 export class PronounsModule {
   private loadPronounData(): any {
     return processFile("pronouns")["data"];
   }
 
-  public getPronoun(sex: string): string {
+  public getPronoun(sex: string): Pronoun {
     const pronouns = this.loadPronounData().find(
       (entry: { [x: string]: any }) => entry[sex]
     );
