e8e13d9c43e348b2201abcbffd54a482982e62f1
Samuel Marques <samuel.s.marques@protonmail.com>
2023-08-31 12:43:57 -0300

    add birthplace

--- src/modules/ethnicities/index.ts
+++ src/modules/ethnicities/index.ts
@@ -9,6 +9,20 @@ export class EthnicitiesModule {
     const ethnicities = this.loadEthnicityData();
     const randomIndex = Math.floor(Math.random() * ethnicities.length);
 
-    return ethnicities[randomIndex];
+    return ethnicities[randomIndex].name;
+  }
+
+  public getRandomBirthplaceFromEthnicity(ethnicity: string): string {
+    const ethnicities = this.loadEthnicityData();
+    const selectedEthnicity = ethnicities.find(
+      (e: { name: string }) => e.name === ethnicity
+    );
+
+    const birthplaces = selectedEthnicity.birthplaces;
+    const randomBirthplaceIndex = Math.floor(
+      Math.random() * birthplaces.length
+    );
+
+    return birthplaces[randomBirthplaceIndex];
   }
 }
