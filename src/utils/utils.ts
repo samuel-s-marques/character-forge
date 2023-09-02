900a189aa0f76ee8d689d8339249b481c5d60005
Samuel Marques <samuel.s.marques@protonmail.com>
2023-09-02 13:38:16 -0300

    Document methods

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -5,6 +5,20 @@ export function generateRandomNumber(min: number, max: number): number {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
+/**
+ * Process a JSON file located in a specific directory.
+ *
+ * This function reads the contents of a JSON file located at a specified path
+ * and parses it into a JavaScript object.
+ *
+ * @param file - The name of the JSON file (without the extension) to be processed.
+ * @returns The JavaScript object parsed from the JSON file.
+ * 
+ * @example
+ * const fileName = "example";
+ * const processedData = processFile(fileName);
+ * // Returns the parsed JavaScript object from the JSON file.
+ */
 export function processFile(file: string): any {
   const filePath = path.join(__dirname, `../../data/${file}/${file}_en.json`);
   const rawData = fs.readFileSync(filePath, "utf-8");
@@ -22,11 +36,48 @@ declare global {
   }
 }
 
+/**
+ * Custom method added to the String prototype for capitalizing the first letter of a string.
+ *
+ * This method capitalizes the first character of the string while preserving the rest of the string.
+ *
+ * @returns {string} - A new string with the first letter capitalized.
+ * @example
+ * const inputString = "hello, world!";
+ * const capitalizedString = inputString.capitalize();
+ * // Returns "Hello, world!"
+ *
+ * const anotherString = "example";
+ * const capitalizedAnotherString = anotherString.capitalize();
+ * // Returns "Example"
+ */
 String.prototype.capitalize = function () {
   return this.charAt(0).toUpperCase() + this.slice(1);
 };
 
-// TODO: Implement multi locale
+/**
+ * Custom method added to the Array prototype for formatting and joining its elements.
+ *
+ * This method joins the elements of the array into a single string with custom formatting:
+ * - If the array contains more than one element, it joins them with commas and replaces the last comma
+ *   with "and" for a grammatically correct list.
+ * - If the array contains exactly one element, it returns that element as is.
+ * - If the array is empty, it returns an empty string.
+ *
+ * @returns A formatted and joined string representation of the array elements.
+ * @example
+ * const fruits = ["apple", "banana", "cherry"];
+ * const formattedString = fruits.formattedJoin();
+ * // Returns "apple, banana, and cherry"
+ *
+ * const singleElementArray = ["orange"];
+ * const singleElementString = singleElementArray.formattedJoin();
+ * // Returns "orange"
+ *
+ * const emptyArray = [];
+ * const emptyString = emptyArray.formattedJoin();
+ * // Returns ""
+ */
 Array.prototype.formattedJoin = function () {
   if (this.length > 1) {
     return this.join(", ").replace(/, ([^,]*)$/, ", and $1");
@@ -37,6 +88,19 @@ Array.prototype.formattedJoin = function () {
   return "";
 };
 
+/**
+ * Returns a weighted random selection from a list of items based
+ * on their respective weights.
+ *
+ * @param items - An array of items to choose from
+ * @param weights - An array of numeric weights corresponding to
+ *                  each item in the `items` array.
+ *                  The sum of all weights should be greater than
+ *                  zero.
+ *
+ * @returns A randomly selected item from the `items` array, with
+ * a probability of selection determined by its weight.
+ */
 export function weightedRandom(items: any[], weights: number[]): any {
   const totalWeight = weights.reduce((sum, weight) => sum + weight, 0);
   const randomValue = Math.random() * totalWeight;
