81f3bd970b5537508efaa2a1faa5bde47b140382
Samuel Marques <samuel.s.marques@protonmail.com>
2023-11-03 14:31:25 -0300

    add `getRandomSkinTone

--- src/modules/ethnicities/index.ts
+++ src/modules/ethnicities/index.ts
@@ -28,10 +28,31 @@ export class EthnicitiesModule {
     );
 
     const birthplaces = selectedEthnicity.birthplaces;
-    const randomBirthplaceIndex = Math.floor(
-      rng.random() * birthplaces.length
-    );
+    const randomBirthplaceIndex = Math.floor(rng.random() * birthplaces.length);
 
     return birthplaces[randomBirthplaceIndex];
+  }
+
+  public getRandomSkinToneFromEthnicity(ethnicity: string): string {
+    const rng = new splitmix32(this.seed);
+    const ethnicities = this.loadEthnicityData();
+    const selectedEthnicity = ethnicities.find(
+      (e: { name: string }) => e.name === ethnicity
+    );
+
+    const skinTones = selectedEthnicity.skin_tones;
+    if (skinTones && skinTones.length > 0) {
+      const totalWeight = skinTones.reduce((sum: any, tone: { weight: any; }) => sum + tone.weight, 0);
+      let randomWeightedIndex = rng.random() * totalWeight;
+
+      for (const tone of skinTones) {
+        randomWeightedIndex -= tone.weight;
+        if (randomWeightedIndex < 0) {
+          return tone.tone;
+        }
+      }
+    }
+
+    return "";
   }
 }
