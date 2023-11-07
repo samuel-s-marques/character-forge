8411f766f78a8effd70eff8fb9d18455e82bbf09
Samuel Marques <samuel.s.marques@protonmail.com>
2023-11-07 00:17:22 -0300

    add `getRandomReligionFromEthnicity`

--- src/modules/ethnicities/index.ts
+++ src/modules/ethnicities/index.ts
@@ -1,5 +1,5 @@
 import { splitmix32 } from "../../utils/splitmix32";
-import { processFile } from "../../utils/utils";
+import { processFile, weightedRandom } from "../../utils/utils";
 
 export class EthnicitiesModule {
   private seed: number | undefined;
@@ -42,7 +42,10 @@ export class EthnicitiesModule {
 
     const skinTones = selectedEthnicity.skin_tones;
     if (skinTones && skinTones.length > 0) {
-      const totalWeight = skinTones.reduce((sum: any, tone: { weight: any; }) => sum + tone.weight, 0);
+      const totalWeight = skinTones.reduce(
+        (sum: any, tone: { weight: any }) => sum + tone.weight,
+        0
+      );
       let randomWeightedIndex = rng.random() * totalWeight;
 
       for (const tone of skinTones) {
@@ -54,5 +57,34 @@ export class EthnicitiesModule {
     }
 
     return "";
+  }
+
+  public getRandomReligionFromEthnicity(ethnicity: string): string {
+    const rng = new splitmix32(this.seed);
+    const ethnicities = this.loadEthnicityData();
+    const selectedEthnicity = ethnicities.find(
+      (e: { name: string }) => e.name === ethnicity
+    );
+
+    const religions = selectedEthnicity.religions;
+    const totalWeight = religions.reduce(
+      (sum: any, religion: { weight: any }) => sum + religion.weight,
+      0
+    );
+
+    const random = rng.random() * totalWeight;
+
+    let selectedReligion = null;
+    let cumulativeWeight = 0;
+
+    for (const religion of religions) {
+      cumulativeWeight += religion.weight;
+      if (random <= cumulativeWeight) {
+        selectedReligion = religion.religion;
+        break;
+      }
+    }
+
+    return selectedReligion;
   }
 }
